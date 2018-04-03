9f0904b50efbacc4e21921b45a3695a0cb83b2d4
petarmarijanovic <petar.marijanovic@q-alliance.com>
2018-04-03 21:17:32 +0200

    Add mapper to MenuChooser

--- src/com/robibp/trening/MenuChooser.java
+++ src/com/robibp/trening/MenuChooser.java
@@ -11,17 +11,31 @@ class MenuChooser {
 
         MenuEnum odabir = null;
         while (odabir == null) {
-            String korisnickiUnos = scanner.nextLine();
+            odabir = toMenuEnum(scanner.nextLine());
 
-            if (korisnickiUnos.equalsIgnoreCase("Unos")) {
-                odabir = MenuEnum.UNOS;
-            } else if (korisnickiUnos.equalsIgnoreCase("Evidencija")) {
-                odabir = MenuEnum.EVIDENCIJA;
-            } else if (korisnickiUnos.equalsIgnoreCase("Izlaz")) {
-                odabir = MenuEnum.IZLAZ;
-            } else {
+            if (odabir == null) {
                 System.out.println("Krivi unos, probajte ponovo");
             }
+        }
+
+        return odabir;
+    }
+
+    /**
+     * Mapper koji mappira String parametar u MenuEnum ili null ako se ne moze mapirati
+     *
+     * @param korisnickiUnos String parametar koju mapiramo u MenuEnum
+     * @return MenuEnum koji smo dobili iz String parametra ili null ako se ne moze mappirati
+     */
+    private MenuEnum toMenuEnum(String korisnickiUnos) {
+        MenuEnum odabir = null;
+
+        if (korisnickiUnos.equalsIgnoreCase("Unos")) {
+            odabir = MenuEnum.UNOS;
+        } else if (korisnickiUnos.equalsIgnoreCase("Evidencija")) {
+            odabir = MenuEnum.EVIDENCIJA;
+        } else if (korisnickiUnos.equalsIgnoreCase("Izlaz")) {
+            odabir = MenuEnum.IZLAZ;
         }
 
         return odabir;
