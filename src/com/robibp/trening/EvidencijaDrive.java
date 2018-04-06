830516ec1c17b8c9352e73ad084702a6614b7c04
RobyBP <robert.baricevicpetrus@gmail.com>
2018-04-06 16:13:54 +0200

    Kreiran object array Klinac za unos Pridodana testna instance variabla Program se ne ponavlja vise puta

--- src/com/robibp/trening/EvidencijaDrive.java
+++ src/com/robibp/trening/EvidencijaDrive.java
@@ -1,19 +1,28 @@
 package com.robibp.trening;
 
+import java.util.Scanner;
+
 class EvidencijaDrive {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         MenuChooser menuChooser = new MenuChooser();
+        Klinac[] imena = new Klinac[100];
 
         switch (menuChooser.askUser()){
             case UNOS:
                 System.out.println("Odabrali ste UNOS!");
+                System.out.println("Unesite ime clana");
+                imena[0] = new Klinac();
+                imena[0].setIme(scanner.nextLine());
+                menuChooser.askUser();
                 break;
             case EVIDENCIJA:
                 System.out.println("Odabrali ste EVIDENCIJA!");
                 break;
             case IZLAZ:
                 System.out.println("Odabrali ste IZLAZ!");
+                System.out.println(imena[0].getIme());
                 return;
         }
     }
---
+++ src/com/robibp/trening/Klinac.java
@@ -0,0 +1,16 @@
+package com.robibp.trening;
+
+public class Klinac {
+    private String ime;
+
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+
+}
