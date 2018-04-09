19408061826ea9da4896a00ce9f364e2fd8e43f3
RobyBP <robert.baricevicpetrus@gmail.com>
2018-04-09 22:11:21 +0200

    Formatiran EvidencijaDrive MenuEnum varijabla ponavljanje preimenovana u odabraniMenu

--- src/com/robibp/trening/EvidencijaDrive.java
+++ src/com/robibp/trening/EvidencijaDrive.java
@@ -8,21 +8,21 @@ class EvidencijaDrive {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         MenuChooser menuChooser = new MenuChooser();
-        MenuEnum ponavljanje = null;
-        while(ponavljanje!= MenuEnum.IZLAZ){
+        MenuEnum odabraniMenu = null;
+        while (odabraniMenu != MenuEnum.IZLAZ) {
 
             switch (menuChooser.askUser()) {
-            case UNOS:
-                System.out.println("Odabrali ste UNOS!");
-                break;
-            case EVIDENCIJA:
-                System.out.println("Odabrali ste EVIDENCIJA!");
-                break;
-            case IZLAZ:
-                System.out.println("Odabrali ste IZLAZ!");
-                ponavljanje = MenuEnum.IZLAZ;
-                break;
-        }
+                case UNOS:
+                    System.out.println("Odabrali ste UNOS!");
+                    break;
+                case EVIDENCIJA:
+                    System.out.println("Odabrali ste EVIDENCIJA!");
+                    break;
+                case IZLAZ:
+                    System.out.println("Odabrali ste IZLAZ!");
+                    odabraniMenu = MenuEnum.IZLAZ;
+                    break;
+            }
         }
     }
 
