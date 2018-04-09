44652185720759af99e99d40e11f653e838970d5
RobyBP <robert.baricevicpetrus@gmail.com>
2018-04-09 22:40:00 +0200

    Inicijalizirana varijabla unutar switch petlje Izbrisana linija koda u case Izlaz

--- src/com/robibp/trening/EvidencijaDrive.java
+++ src/com/robibp/trening/EvidencijaDrive.java
@@ -1,6 +1,5 @@
 package com.robibp.trening;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 class EvidencijaDrive {
@@ -8,10 +7,10 @@ class EvidencijaDrive {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         MenuChooser menuChooser = new MenuChooser();
-        MenuEnum odabraniMenu = null;
+        MenuEnum odabraniMenu = menuChooser.askUser();
         while (odabraniMenu != MenuEnum.IZLAZ) {
-
-            switch (menuChooser.askUser()) {
+            odabraniMenu = menuChooser.askUser();
+            switch (odabraniMenu) {
                 case UNOS:
                     System.out.println("Odabrali ste UNOS!");
                     break;
@@ -20,7 +19,6 @@ class EvidencijaDrive {
                     break;
                 case IZLAZ:
                     System.out.println("Odabrali ste IZLAZ!");
-                    odabraniMenu = MenuEnum.IZLAZ;
                     break;
             }
         }
